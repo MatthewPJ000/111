f9921edca2571df90ab85e6193f27ff4c512784b
Matthew Jack <matthewjack388@gmail.com>
2024-11-21 11:09:43 -0800

    Update modalBox.tsx

--- src/components/modalBox.tsx
+++ src/components/modalBox.tsx
@@ -153,7 +153,7 @@ const InputsBox: React.FC<InputsBoxProps> = ({
               placeholder="Type Title"
               value={data ? data.title : ""}
               onChange={(e) => handleTitleChange(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             />
           </div>
           <label className="block mb-2 text-2xl font-medium">
